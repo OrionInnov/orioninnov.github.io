40869c79f53d5d4583187ac71fafc13697def027
jkddw <1561068317@qq.com>
2020-07-23 11:49:18 +0800

    Some changes.

--- js/language.js
+++ js/language.js
@@ -52,10 +52,10 @@ function setChinese() {
   $("#section3Txt1").html("我们的系统");
   $("#section3Txt2").html("奥新智能室内定位系统");
   $("#section01Txt1").html("我们的产品");
-  $("#section01Txt2").html("OSDR Q10");
+  $("#section01Txt2").html("软件定义无线电");
   $("#section01Txt3").html("OSDR S10");
-  $("#section01Txt4").html("天线");
-  $("#section01Txt5").html("定制外壳");
+  $("#section01Txt4").html("智能安全帽");
+  $("#section01Txt5").html("智能安全帽");
   $("#section5Txt1").html("联系我们");
   $('#fname').attr('placeholder','姓名');
   $('#lname').attr('placeholder','邮箱');
@@ -81,9 +81,9 @@ function setChinese() {
   //Product1
 
   $("#product1").html("<h6>产品介绍</h6><h2>高性能软件定义无线电</h2><h4>OSDR Q10是Orion Innovations衍生的软件定义无线电产品之一，能够通过四个12位完全同步的通道发送和接收任何宽带RF信号。OSDR Q10主要面向高级用户和/或研究组，而OSDR S10更适合简单应用。</h4><h4>点击<a href='https://m.tb.cn/h.e904tOp?sm=08bdf2' class='weblink'>这里</a>购买该产品.</h4>");
-  $("#product2").html("<h6>产品介绍</h6><h2>OSDR S10</h2><h4>OSDR S10是一套更具性价比的软件定义无线电解决方案。</h4>");
-  $("#product3").html("<h6>产品介绍</h6><h2>高性能天线和其他配套器件</h2><h4>我们拥有支持2.4G和5G的高增益天线以应对各种场景。</h4>");
-  $("#product4").html("<h6>产品介绍</h6><h2>3D打印外壳</h2><h4>你可以通过3D打印为OSDR定制外壳。</h4>");
+  $("#product11").html("<h6>产品介绍</h6><h2>OSDR S10</h2><h4>OSDR S10是一套更具性价比的软件定义无线电解决方案(即将面世)。</h4>");
+  $("#product2").html("<h6>产品介绍</h6><h2>高性能定位标签</h2><h4>我们拥有用来进行定位的高性能定位标签。该标签可以发射高强度的信号，具有极强的穿墙能力，可用于各种复杂场景。产品包括多种传感器，除了2d位置信息，客户还可以通过它得知楼层，温度，湿度，方向等信息。</h4>");
+  $("#product21").html("<h6>产品介绍</h6><h2>基站</h2><h4>我们拥有基站以接收标签信息。该产品部署简单，可以在定位空间外任意地方摆放3个或者4个，用以接收标签的信号，不需要任何线缆，随时部署，随时切换场地，使得不确定性场地的室内定位（比如消防应用）成为可能。</h4>");
 
 
 }
@@ -103,10 +103,10 @@ function setEnglish() {
   $("#section3Txt1").html("Our System");
   $("#section3Txt2").html("Orion Indoor Position System");
   $("#section01Txt1").html("Our Products");
-  $("#section01Txt2").html("OSDR Q10");
+  $("#section01Txt2").html("Software defined radio");
   $("#section01Txt3").html("OSDR Q10");
   $("#section01Txt4").html("Antenna");
-  $("#section01Txt5").html("Custom-made Shell");
+  $("#section01Txt5").html("Smart helmet");
   $("#section5Txt1").html("Contact Us");
   $('#fname').attr('placeholder','Name');
   $('#lname').attr('placeholder','Email Address');
@@ -132,9 +132,9 @@ function setEnglish() {
   //Product
 
   $("#product1").html("<h6>Product Description</h6><h2>High performance software defined radio</h2><h4>The OSDR Q10 is one of Orion Innovations' derivative software defined radio products, capable of transmitting and receiving any wideband RF signal via four 12-bit fully synchronized channels. The OSDR Q10 is intended primarily for advanced users and/or research groups, while the OSDR S10 is more suitable for simple applications.</h4><h4>Click <a href='http://oriontech.io/' class='weblink'>here</a> to buy this product.</h4>");
-  $("#product2").html("<h6>Product Description</h6><h2>OSDR S10</h2><h4>OSDR S10 is a more cost-effective solution about software defined radio.</h4>");
-  $("#product3").html("<h6>Product Description</h6><h2>High performance antenna and other accessories</h2><h4>We have antennas with high gain, 2.4G and 5G supported to deal with various scenarios.</h4>");
-  $("#product4").html("<h6>Product Description</h6><h2>3D Printed Shell</h2><h4>You can get Custom-made Shell by 3d printing.</h4>");
+  $("#product11").html("<h6>Product Description</h6><h2>OSDR S10</h2><h4>OSDR S10 is a more cost-effective solution about software defined radio(Coming soon).</h4>");
+  $("#product2").html("<h6>Product Description</h6><h2>High performance position tag</h2><h4>We have smart tag to fix position.The tag can emit high-intensity signals, has a strong ability to penetrate walls, and can be used in various complex scenarios. The product includes a variety of sensors. In addition to 2d location information, customers can also learn floor, temperature, humidity, direction and other information through it.</h4>");
+  $("#product21").html("<h6>Product Description</h6><h2>Base station</h2><h4>We have base station to receive information of tags.The product is simple to deploy. It can be placed anywhere outside of the positioning space 3 or 4 to receive the signal of the tag. It does not need any cables. It can be deployed at any time and switch sites at any time, making the indoor positioning of uncertain sites (such as Fire application) becomes possible.</h4>");
 }
 
 window.onload = function() {
